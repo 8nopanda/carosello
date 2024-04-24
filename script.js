57bcf78cc6d463ed8d5f7464cb4b713855c0f49e
Salvatore Bisconti <121030864+SalvoBisconti@users.noreply.github.com>
2024-04-24 03:07:16 +0200

    add

--- script.js
+++ script.js
@@ -9,8 +9,15 @@ let durataFilm = qS("#durataFilm");
 let annoUscita = qS("#annoUscita");
 let genereFilm = qS("#genereFilm");
 let inputCerca = qS(".inputCerca");
+let formCercaFilm = qS(".formCercaFilm");
 let tramaFilm = qS(".tramaFilm");
 let immagineCercata = qS(".immagineCercata");
+let titoloCerca = qS("#titoloCerca");
+let carosello = qS(".carosello");
+let primoAttore = qS("#primoAttore");
+let secondoAttore = qS("#secondoAttore");
+let terzoAttore = qS("#terzoAttore");
+let tramaTitolo = qS(".tramaTitolo");
 
 const URLS_API = [
   "https://www.omdbapi.com/?t=John+Wick&apikey=4214e970",
@@ -21,24 +28,16 @@ const URLS_API = [
 let dataFilm = [];
 let i = 0;
 
-const nextImage = () => {
-  i++;
-  if (i > 2) {
-    i = 0;
-  } else {
-    i++;
-  }
-  console.log(i);
-};
-
-const prevImage = () => {
-  i--;
-  if (i < 0) {
-    i = 2;
-  } else {
-    i++;
-  }
-  console.log(i);
+const popolaCarosello = () => {
+  let actorArray = dataFilm[i].Actors.split(", ");
+  titoloFilm.textContent = dataFilm[i].Title;
+  immagineCarosello.src = dataFilm[i].Poster;
+  primoAttore.textContent = actorArray[0];
+  secondoAttore.textContent = actorArray[1];
+  terzoAttore.textContent = actorArray[2];
+  durataFilm.textContent = `${dataFilm[i].Runtime.split(" ")[0]} minuti`;
+  annoUscita.textContent = dataFilm[i].Year;
+  genereFilm.textContent = dataFilm[i].Genre;
 };
 
 URLS_API.forEach((element) => {
@@ -46,22 +45,37 @@ URLS_API.forEach((element) => {
     .then((response) => response.json())
     .then((data) => {
       dataFilm.push(data);
-      popolaCarosello(dataFilm);
+      popolaCarosello();
     });
 });
 
-const popolaCarosello = (film) => {
-  let listItem = document.createElement("li");
-  titoloFilm.textContent = film[i].Title;
-  immagineCarosello.src = film[i].Poster;
-  film[i].Actors.split(", ").forEach((element, index) => {
-    listItem.textContent = element;
-    listItem.id = index;
-    attoriList.appendChild(listItem);
+document.addEventListener("DOMContentLoaded", () => {
+  btnNext.addEventListener("click", (e) => {
+    i++;
+    if (i > 2) {
+      i = 0;
+    }
+    popolaCarosello();
   });
-  durataFilm.textContent = `${film[i].Runtime.split(" ")[0]} minuti`;
-  annoUscita.textContent = film[i].Year;
-  genereFilm.textContent = film[i].Genre;
-};
 
-console.log(i);
+  btnPrev.addEventListener("click", (e) => {
+    i--;
+    if (i < 0) {
+      i = 2;
+    }
+    popolaCarosello();
+  });
+});
+
+formCercaFilm.addEventListener("click", (e) => {
+  e.preventDefault();
+  fetch(`https://www.omdbapi.com/?t=${inputCerca.value}&apikey=4214e970`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      tramaFilm.textContent = data.Plot;
+      immagineCercata.src = data.Poster;
+      titoloCerca.textContent = data.Title;
+      tramaTitolo.style.display = "block";
+    });
+});
